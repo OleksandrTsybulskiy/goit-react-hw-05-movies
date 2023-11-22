e2e49a070cdfe96a05a80e1ab7437b6ce541784a
OleksandrTsybulskiy <126475884+OleksandrTsybulskiy@users.noreply.github.com>
2023-11-22 19:39:06 +0200

    hw5

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,9 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import Layout from './Layout/Layout';
 import { lazy } from 'react';
 
-const Home = lazy(() => import ('../pages/Home/Home'))
-const Movies = lazy(() => import ('../pages/Movies/Movies'))
-const MovieDetails = lazy(() => import ('../pages/MovieDetails/MovieDetails'))
+const Home = lazy(() => import ('../Pages/Home/Home'))
+const Movies = lazy(() => import ('../Pages/Movies/Movies'))
+const MovieDetails = lazy(() => import ('../Pages/MovieDetails/MovieDetails'))
 const Cast = lazy(() => import ('../components/Cast/Cast'))
 const Reviews = lazy(() => import ('../components/Reviews/Reviews'))
 
